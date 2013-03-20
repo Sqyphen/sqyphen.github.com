f081ba69b5a48220051540fe00d5334a9ecef48c
Tobias Fieldhouse <tobias.fieldhouse@gmail.com>
2013-03-20 14:28:47 +0000

    added labs folder with first project - photobooth

---
+++ Documents/git/sqyphen.github.com/labs/photobooth/assets/js/plugins.js
@@ -0,0 +1,27 @@
+// Avoid `console` errors in browsers that lack a console.
+if (!(window.console && console.log)) {
+    (function() {
+        var noop = function() {};
+        var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
+        var length = methods.length;
+        var console = window.console = {};
+        while (length--) {
+            console[methods[length]] = noop;
+        }
+    }());
+}
+
+
+//puts GET variables into array
+var $_GET = {};
+
+document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
+    function decode(s) {
+        return decodeURIComponent(s.split("+").join(" "));
+    }
+
+    $_GET[decode(arguments[1])] = decode(arguments[2]);
+});
+
+
+// Place any further jQuery/helper plugins in here.
